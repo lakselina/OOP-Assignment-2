2b582381c260a180ae5c361b1d6564efa1077636
Itay <avitit@post.bgu.ac.il>
2026-05-28 22:16:15 +0300

    'working on monster logic'

--- src/units/enemy/Enemy.java
+++ src/units/enemy/Enemy.java
@@ -1,4 +1,27 @@
 package units.enemy;
+import units.Unit;
+import units.player.Player;
+import visitor.OccupantVisitor;
+import utils.Point;
 
-public class Enemy {
+
+public abstract class Enemy extends Unit {
+    protected Integer experienceValue;
+
+
+    public Enemy(Point point, String name, Integer healthPool, Integer healthAmount, Integer attackPoints, Integer defensePoints, Integer experienceValue) {
+        super(point, name, healthPool, healthAmount, attackPoints, defensePoints);
+        this.experienceValue = experienceValue;
+    }
+
+    public Integer getExperienceValue(){
+        return this.experienceValue;
+    }
+
+    public void accept(OccupantVisitor visitor){
+        visitor.visit(this);
+    }
+
+    public abstract void onEnemyTurn(Player player);
 }
+
--- src/units/enemy/Monster.java
+++ src/units/enemy/Monster.java
@@ -1,4 +1,14 @@
 package units.enemy;
 
-public class Monster {
+import utils.Point;
+
+public class Monster extends Enemy{
+    private Integer visionRange;
+
+    public Monster(Point point, String name, Integer healthPool, Integer healthAmount, Integer attackPoints, Integer defensePoints, Integer experienceValue, Integer visionRange){
+        super(point, name, healthPool, healthAmount, attackPoints, defensePoints, experienceValue);
+        this.visionRange = visionRange;
+    }
+
+
 }
